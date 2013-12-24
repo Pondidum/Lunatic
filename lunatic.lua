0e1fcb19223c0ba817c9b4bcd77a6a3f2a1e4950
andy <andy@andydote.co.uk>
2013-12-24 16:28:06 +0000

    testRunner: add runSet, which runs all functions in a table, and handlers before+after

--- lunatic.lua
+++ lunatic.lua
@@ -1,34 +1,51 @@
 
-local testRunner = {
+local testRunner = {}
 
-	run = function(before, test, after)
+testRunner.run = function(before, test, after)
 
-		local old = getfenv()
+	local old = getfenv()
 
-		setfenv(1, setmetatable({}, { __index = old }))
+	setfenv(1, setmetatable({}, { __index = old }))
 
-		local action = function()
+	local action = function()
 
-			if before then
-				pcall(before)
-			end
-
-			local success, e = pcall(test)
-
-			if after then
-				after()
-			end
-
-			return success, e
-
+		if before then
+			pcall(before)
 		end
 
-		local s, e = action()
+		local success, e = pcall(test)
 
-		setfenv(1, old)
+		if after then
+			after()
+		end
 
-		return s, e
+		return success, e
 
-	end,
+	end
 
-}
+	local s, e = action()
+
+	setfenv(1, old)
+
+	return s, e
+
+end
+
+testRunner.runSet = function(set)
+
+	local before = set.before
+	local after = set.after
+
+	local result = {}
+
+	for k,v in pairs(set) do
+
+		if k ~= "before" and k ~= "after" then
+			result[k] = testRunner.run(before, v, after)
+		end
+
+	end
+
+	return result
+
+end
