6aa66a6d25e51ad8ab0a0d813379e5dbf736bb7e
andy <andy@andydote.co.uk>
2013-12-26 23:39:06 +0000

    lunatic: added assertions, alias them as 'should' for names to make sense

--- lunatic.lua
+++ lunatic.lua
@@ -55,13 +55,85 @@ testRunner.runSet = function(set)
 
 end
 
+local should = {
+
+	haveCount = function(expected, collection, message)
+
+		local actual = 0
+
+		for k,v in pairs(collection) do
+			actual = actual + 1
+		end
+
+		assert(actual == expected, string.format(message or "Expected %d elements, but there were %d.", expected, actual))
+
+	end,
+
+	equal = function(expected, actual, message)
+
+		assert(actual == expected, string.format(message or "Expected %s and %s to be the same, but were different.", expected, actual))
+
+	end,
+
+	notEqual = function(expected, actual, message)
+
+		assert(actual ~= expected, string.format(message or "Expected %s and %s to be different, but were the same.", expected, actual))
+
+	end,
+
+	containKey = function(item, collection, message)
+
+		assert(collection[item], string.format(message or "Expected the key %s to exist, but didn't.", item))
+
+	end,
+
+	notContainKey = function(item, collection, message)
+
+		assert(collection[item] == nil, string.format(message or "Expected the key %s to exist, but didn't.", item))
+
+	end,
+
+	containValue = function(item, collection, message)
+
+		local inCollection = false
+
+		for k,v in pairs(collection) do
+			if v == item then
+				inCollection = true
+			end
+		end
+
+		assert(inCollection, string.format(message or "Expected %s to be in the collection, but it wasn't.", item))
+
+	end,
+
+	notContainValue = function(item, collection, message)
+
+		local inCollection = false
+
+		for k,v in pairs(collection) do
+			if v == item then
+				inCollection = true
+			end
+		end
+
+		assert(inCollection == false, string.format(message or "Expected %s to not be in the collection, but it was.", item))
+
+	end,
+
+}
+
 local testEngine = {
+
+	should = should,
 
 	new = function()
 
 		local this = {}
 		local sets = {}
 		local results = {}
+
+		this.should = should
 
 		this.add = function(name, set)
 
